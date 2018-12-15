da804a8358ca07c99a3209f9136c417f238b5302
Bruno G. A. Lebtag <brlebtag@gmail.com>
2018-12-15 17:07:39 +0000

    Prepara ambiente do frontend e ajusta gulp/webpack.

--- frontend/Gulpfile.js
+++ frontend/Gulpfile.js
@@ -1,1 +1,65 @@
-const { series } = require('gulp');
+'use strict';
+
+const { series, src, dest, parallel } = require('gulp');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const webpack = require('webpack-stream');
+
+sass.compiler = require('node-sass');
+
+function limparCompilados(cb) {
+    return src([
+            './public/js/*.js',
+            './public/css/*.css'
+        ])
+        .pipe(clean());
+}
+
+function compilarJs() {
+    return src('./resources/js/index.js')
+        .pipe(webpack({
+            mode: 'development',
+            module: {
+                rules: [{
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                        presets: ['@babel/preset-env']
+                        }
+                    }
+                }]
+            }
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./public/js'));
+}
+
+function compilarCss() {
+    const plugins = [
+        autoprefixer({browsers: ['last 1 version']}),
+        cssnano()
+    ];
+
+    const sassStream = src('./resources/scss/index.scss')
+        .pipe(sass().on('error', sass.logError));
+
+    const cssStream = src('./resources/css/*.css');
+
+    return merge(sassStream, cssStream)
+        .pipe(concat('style.css'))
+        .pipe(postcss(plugins))
+        .pipe(dest('./public/css'));
+}
+
+exports.default =
+    series(
+        limparCompilados,
+        parallel(compilarJs, compilarCss)
+    );
---
+++ frontend/resources/js/index.js
@@ -0,0 +1,12 @@
+window._ = require('lodash');
+window.Popper = require('popper.js').default;
+
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+window.axios = require('axios');
+
+window.angular = require('angular');
